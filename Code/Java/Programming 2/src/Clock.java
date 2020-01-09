1c0dd8e54f7e5d3efc561bb4e0849ab0b7aa15dd
nicholasz2510 <nicholasz2510@gmail.com>
2020-01-09 07:36:34 -0700

    Updated Clock.java

--- Code/Java/Programming 2/src/Clock.java
+++ Code/Java/Programming 2/src/Clock.java
@@ -1,2 +1,21 @@
 public class Clock {
+    public String getHours() {
+        return java.time.LocalTime.now().toString().substring(0, 2);
+    }
+
+    public String getMinutes() {
+        return java.time.LocalTime.now().toString().substring(3, 5);
+    }
+
+    public String getTime() {
+        return getHours() + ":" + getMinutes();
+    }
+
+    public static void main(String[] args) {
+        Clock c = new Clock();
+
+        System.out.println(c.getHours());
+        System.out.println(c.getMinutes());
+        System.out.println(c.getTime());
+    }
 }
---
+++ Code/Java/Programming 2/src/WorldClock.java
@@ -0,0 +1,2 @@
+public class WorldClock extends Clock {
+}
