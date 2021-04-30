45746a47985120cbdb03825dd78ede78871d4aed
Nicholas Zhang <nicholasz2510@gmail.com>
2021-04-30 12:10:36 -0600

    Make IA Downloader more general

--- Code/Python3/unus_annus_downloader.py
+++
@@ -1,7 +0,0 @@
-from internetarchive import download
-from internetarchive import configure
-
-login = open("ia_login.txt", "r")
-
-configure(login.readline().strip(), login.readline().strip())
-download('full-unus-annus-archive-2', verbose=True, destdir="C:/Users/nicho/Videos/UNUS ANNUS", ignore_errors=True, ignore_existing=True)
