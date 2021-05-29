04947fb9166165d9ea5352e55df391c6de2eac8a
Nicholas Zhang <nicholasz2510@gmail.com>
2021-05-29 15:43:15 -0600

    remove

--- Code/Python3/FamilySearchDownload/downloader.py
+++
@@ -1,9 +0,0 @@
-from internetarchive import download
-from internetarchive import configure
-
-login = open("ia_login.txt", "r")
-configure(login.readline().strip(), login.readline().strip())
-
-identifier = input("What is the Identifier of the archive? ").strip()
-
-download(identifier, formats=["Cue Sheet", "Flac"], verbose=True, destdir="downloads", retries=2, ignore_errors=True, ignore_existing=True)
